96d719f8b02bfb997d4df4d141eb7d3a3ffca210
martin-bobek <martinbobek8@gmail.com>
2019-05-30 23:30:44 -0600

    Created defines for LEDs.
    Added defines representing the pin number and bit mask for the 4 dev
    board LEDs.

--- source/main.c
+++ source/main.c
@@ -2,6 +2,16 @@
 
 #define BIT(n) (uint32_t)(1u << n)
 
+#define LED1_PIN		17
+#define LED2_PIN		18
+#define LED3_PIN		19
+#define LED4_PIN		20
+
+#define LED1			BIT(LED1_PIN)
+#define LED2			BIT(LED2_PIN)
+#define LED3			BIT(LED3_PIN)
+#define LED4			BIT(LED4_PIN)
+
 int main(void) {
-    NRF_P0->DIRSET = BIT(17) | BIT(18) | BIT(19) | BIT(20);
+    NRF_P0->DIRSET = LED1 | LED2 | LED3 | LED4;
 }
