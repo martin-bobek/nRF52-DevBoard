7e161d57290e2d1df64ca80106cefc7a22db4801
martin-bobek <martinbobek8@gmail.com>
2019-06-08 19:22:06 -0600

    Enabled pull-ups and pull-downs.
    Enabled pull-ups on the 4 UART lines to the interface MCU and RESET.
    Enabled pull-downs on all other unused GPIO.

--- source/init.c
+++ source/init.c
@@ -1,21 +1,24 @@
 #include <nrf.h>
 #include "init.h"
 
-#define INPUT_DISC       MSK( GPIO_PIN_CNF_INPUT_Disconnect, GPIO_PIN_CNF_INPUT_Pos )
+#define INDSC_PLUP      (MSK( GPIO_PIN_CNF_INPUT_Disconnect, GPIO_PIN_CNF_INPUT_Pos ) | \
+                         MSK( GPIO_PIN_CNF_PULL_Pullup,      GPIO_PIN_CNF_PULL_Pos  ))
+#define INDSC_PLDWN     (MSK( GPIO_PIN_CNF_INPUT_Disconnect, GPIO_PIN_CNF_INPUT_Pos ) | \
+                         MSK( GPIO_PIN_CNF_PULL_Pulldown,    GPIO_PIN_CNF_PULL_Pos  ))
 #define LED_PIN_CNF     (MSK( GPIO_PIN_CNF_INPUT_Disconnect, GPIO_PIN_CNF_INPUT_Pos ) | \
                          MSK( GPIO_PIN_CNF_DRIVE_H0S1,       GPIO_PIN_CNF_DRIVE_Pos ))
 
 #define SYSTICK_250MS   (16000000u - 1u)
 
 const uint32_t GPIO_CNF[32] = {
-//      P0.00       P0.01        P0.02        P0.03        P0.04        P0.05       P0.06       P0.07
-        INPUT_DISC, INPUT_DISC,  INPUT_DISC,  INPUT_DISC,  INPUT_DISC,  INPUT_DISC, INPUT_DISC, INPUT_DISC,
-//      P0.08       P0.09        P0.10        P0.11        P0.12        P0.13       P0.14       P0.15
-        INPUT_DISC, INPUT_DISC,  INPUT_DISC,  INPUT_DISC,  INPUT_DISC,  INPUT_DISC, INPUT_DISC, INPUT_DISC,
-//      P0.16       P0.17        P0.18        P0.19        P0.20        P0.21       P0.22       P0.23
-        INPUT_DISC, LED_PIN_CNF, LED_PIN_CNF, LED_PIN_CNF, LED_PIN_CNF, INPUT_DISC, INPUT_DISC, INPUT_DISC,
-//      P0.24       P0.25        P0.26        P0.27        P0.28        P0.29       P0.30       P0.31
-        INPUT_DISC, INPUT_DISC,  INPUT_DISC,  INPUT_DISC,  INPUT_DISC,  INPUT_DISC, INPUT_DISC, INPUT_DISC };
+//      P0.00        P0.01        P0.02        P0.03        P0.04        P0.05 CTS    P0.06 RxD    P0.07 RTS
+        INDSC_PLDWN, INDSC_PLDWN, INDSC_PLDWN, INDSC_PLDWN, INDSC_PLDWN, INDSC_PLUP,  INDSC_PLUP,  INDSC_PLUP,
+//      P0.08 TxD    P0.09 NFC1   P0.10 NFC2   P0.11        P0.12        P0.13 BTN1   P0.14 BTN2   P0.15 BTN3
+        INDSC_PLUP,  INDSC_PLDWN, INDSC_PLDWN, INDSC_PLDWN, INDSC_PLDWN, INDSC_PLDWN, INDSC_PLDWN, INDSC_PLDWN,
+//      P0.16 BTN4   P0.17 LED1   P0.18 LED2   P0.19 LED3   P0.20 LED4   P0.21 RESET  P0.22        P0.23
+        INDSC_PLDWN, LED_PIN_CNF, LED_PIN_CNF, LED_PIN_CNF, LED_PIN_CNF, INDSC_PLUP,  INDSC_PLDWN, INDSC_PLDWN,
+//      P0.24        P0.25        P0.26        P0.27        P0.28        P0.29        P0.30        P0.31
+        INDSC_PLDWN, INDSC_PLDWN, INDSC_PLDWN, INDSC_PLDWN, INDSC_PLDWN, INDSC_PLDWN, INDSC_PLDWN, INDSC_PLDWN };
 
 void Init(void) {
     for (uint8_t pin = 0; pin < 32; pin++)
