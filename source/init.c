9e271a83b4ecc251d754960389819d5cba0fc42e
martin-bobek <martinbobek8@gmail.com>
2019-06-24 23:24:50 -0600

    Split up Init into several smaller functions.
    Divided Init into several smaller functions, grouped based on
    peripheral. Each smaller inline function is called from Init.

--- source/init.c
+++ source/init.c
@@ -19,7 +19,7 @@
 
 #define RTC_PRESCALER   4095u
 
-const uint32_t GPIO_CNF[32] = {
+static const uint32_t GPIO_CNF[32] = {
 //      P0.00 XL1    P0.01 XL2    P0.02        P0.03        P0.04        P0.05 CTS    P0.06 RxD    P0.07 RTS
         INDSK_NOPL,  INDSK_NOPL,  INDSC_PLDWN, INDSC_PLDWN, INDSC_PLDWN, INDSC_PLUP,  INDSC_PLUP,  INDSC_PLUP,
 //      P0.08 TxD    P0.09 NFC1   P0.10 NFC2   P0.11        P0.12        P0.13 BTN1   P0.14 BTN2   P0.15 BTN3
@@ -29,23 +29,44 @@ const uint32_t GPIO_CNF[32] = {
 //      P0.24        P0.25        P0.26        P0.27        P0.28        P0.29        P0.30        P0.31
         INDSC_PLDWN, INDSC_PLDWN, INDSC_PLDWN, INDSC_PLDWN, INDSC_PLDWN, INDSC_PLDWN, INDSC_PLDWN, INDSC_PLDWN };
 
+static inline void PowerInit(void) __attribute((always_inline));
+static inline void ErrataInit(void) __attribute((always_inline));
+static inline void GpioInit(void) __attribute((always_inline));
+static inline void ClockInit(void) __attribute((always_inline));
+static inline void RtcInit(void) __attribute((always_inline));
+static inline void IsrInit(void) __attribute((always_inline));
+
 void Init(void) {
+    PowerInit();
+    ErrataInit();
+    GpioInit();
+    ClockInit();
+    RtcInit();
+    IsrInit();
+}
+
+void PowerInit(void) {
     NRF_POWER->DCDCEN = DCDC_ENABLED;
-
+}
+void ErrataInit(void) {
     ERRATA108_DST = ERRATA108_SRC & ERRATA108_MSK;
-
+}
+void GpioInit(void) {
     for (uint8_t pin = 0; pin < 32; pin++)
         NRF_P0->PIN_CNF[pin] = GPIO_CNF[pin];
 
     NRF_P0->OUT = LED_ALL;
     NRF_P0->DIRSET = LED_ALL;
-
+}
+void ClockInit(void) {
     NRF_CLOCK->LFCLKSRC = CLKSRC_XTAL;
     NRF_CLOCK->TASKS_LFCLKSTART = 1;
-
+}
+void RtcInit(void) {
     NRF_RTC0->PRESCALER = RTC_PRESCALER;
     NRF_RTC0->INTENSET = RTC_INTENSET_TICK_Msk;
     NRF_RTC0->TASKS_START = 1;
-
+}
+void IsrInit(void) {
     NVIC->ISER[0] = BIT(RTC0_IRQn);
 }
