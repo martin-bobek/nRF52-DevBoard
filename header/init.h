0a97b558950babe28079ff9278088275619ab9cc
martin-bobek <martinbobek8@gmail.com>
2019-06-29 13:13:05 -0600

    Adjusted GPIO init for Tx-only UART.
    Defined the four pins used for UART. Enabled TxD as a high-drive output.
    Setup the remaining 3 pins to not cause any conflicts when they are
    unused.

--- header/init.h
+++ header/init.h
@@ -4,11 +4,19 @@
 #define MSK(value, shift)   (uint32_t)((value) << (shift))
 #define BIT(n)              MSK(1, n)
 
+#define RTS_PIN         5
+#define TXD_PIN         6
+#define CTS_PIN         7
+#define RXD_PIN         8
 #define LED1_PIN        17
 #define LED2_PIN        18
 #define LED3_PIN        19
 #define LED4_PIN        20
 
+#define UART_RTS        BIT(RTS_PIN)
+#define UART_TXD        BIT(TXD_PIN)
+#define UART_CTS        BIT(CTS_PIN)
+#define UART_RXD        BIT(RXD_PIN)
 #define LED1            BIT(LED1_PIN)
 #define LED2            BIT(LED2_PIN)
 #define LED3            BIT(LED3_PIN)
--- source/init.c
+++ source/init.c
@@ -7,23 +7,24 @@
 #define ERRATA108_SRC   REG(0x10000258)
 #define ERRATA108_MSK   0x0000004F
 
-#define INDSK_NOPL       MSK( GPIO_PIN_CNF_INPUT_Disconnect, GPIO_PIN_CNF_INPUT_Pos )
-#define INDSC_PLUP      (MSK( GPIO_PIN_CNF_INPUT_Disconnect, GPIO_PIN_CNF_INPUT_Pos ) | \
-                         MSK( GPIO_PIN_CNF_PULL_Pullup,      GPIO_PIN_CNF_PULL_Pos  ))
-#define INDSC_PLDWN     (MSK( GPIO_PIN_CNF_INPUT_Disconnect, GPIO_PIN_CNF_INPUT_Pos ) | \
-                         MSK( GPIO_PIN_CNF_PULL_Pulldown,    GPIO_PIN_CNF_PULL_Pos  ))
-#define LED_PIN_CNF     (MSK( GPIO_PIN_CNF_INPUT_Disconnect, GPIO_PIN_CNF_INPUT_Pos ) | \
-                         MSK( GPIO_PIN_CNF_DRIVE_H0S1,       GPIO_PIN_CNF_DRIVE_Pos ))
+#define HIDRV_BOTH       MSK( GPIO_PIN_CNF_DRIVE_H0H1,       GPIO_PIN_CNF_DRIVE_Pos  )
+#define INDSC_NOPL       MSK( GPIO_PIN_CNF_INPUT_Disconnect, GPIO_PIN_CNF_INPUT_Pos  )
+#define INDSC_PLUP      (MSK( GPIO_PIN_CNF_INPUT_Disconnect, GPIO_PIN_CNF_INPUT_Pos  ) | \
+                         MSK( GPIO_PIN_CNF_PULL_Pullup,      GPIO_PIN_CNF_PULL_Pos   ))
+#define INDSC_PLDWN     (MSK( GPIO_PIN_CNF_INPUT_Disconnect, GPIO_PIN_CNF_INPUT_Pos  ) | \
+                         MSK( GPIO_PIN_CNF_PULL_Pulldown,    GPIO_PIN_CNF_PULL_Pos   ))
+#define LED_PIN_CNF     (MSK( GPIO_PIN_CNF_INPUT_Disconnect, GPIO_PIN_CNF_INPUT_Pos  ) | \
+                         MSK( GPIO_PIN_CNF_DRIVE_H0S1,       GPIO_PIN_CNF_DRIVE_Pos  ))
 #define DCDC_ENABLED     MSK( POWER_DCDCEN_DCDCEN_Enabled,   POWER_DCDCEN_DCDCEN_Pos )
-#define CLKSRC_XTAL      MSK( CLOCK_LFCLKSRC_SRC_Xtal,   CLOCK_LFCLKSRC_SRC_Pos )
+#define CLKSRC_XTAL      MSK( CLOCK_LFCLKSRC_SRC_Xtal,       CLOCK_LFCLKSRC_SRC_Pos  )
 
 #define RTC_PRESCALER   4095u
 
 static const uint32_t GPIO_CNF[32] = {
-//      P0.00 XL1    P0.01 XL2    P0.02        P0.03        P0.04        P0.05 CTS    P0.06 RxD    P0.07 RTS
-        INDSK_NOPL,  INDSK_NOPL,  INDSC_PLDWN, INDSC_PLDWN, INDSC_PLDWN, INDSC_PLUP,  INDSC_PLUP,  INDSC_PLUP,
-//      P0.08 TxD    P0.09 NFC1   P0.10 NFC2   P0.11        P0.12        P0.13 BTN1   P0.14 BTN2   P0.15 BTN3
-        INDSC_PLUP,  INDSC_PLDWN, INDSC_PLDWN, INDSC_PLDWN, INDSC_PLDWN, INDSC_PLDWN, INDSC_PLDWN, INDSC_PLDWN,
+//      P0.00 XL1    P0.01 XL2    P0.02        P0.03        P0.04        P0.05 RTS    P0.06 TxD    P0.07 CTS
+        INDSC_NOPL,  INDSC_NOPL,  INDSC_PLDWN, INDSC_PLDWN, INDSC_PLDWN, INDSC_PLDWN, HIDRV_BOTH,  INDSC_NOPL,
+//      P0.08 RxD    P0.09 NFC1   P0.10 NFC2   P0.11        P0.12        P0.13 BTN1   P0.14 BTN2   P0.15 BTN3
+        INDSC_NOPL,  INDSC_PLDWN, INDSC_PLDWN, INDSC_PLDWN, INDSC_PLDWN, INDSC_PLDWN, INDSC_PLDWN, INDSC_PLDWN,
 //      P0.16 BTN4   P0.17 LED1   P0.18 LED2   P0.19 LED3   P0.20 LED4   P0.21 RESET  P0.22        P0.23
         INDSC_PLDWN, LED_PIN_CNF, LED_PIN_CNF, LED_PIN_CNF, LED_PIN_CNF, INDSC_PLUP,  INDSC_PLDWN, INDSC_PLDWN,
 //      P0.24        P0.25        P0.26        P0.27        P0.28        P0.29        P0.30        P0.31
@@ -55,8 +56,8 @@ void GpioInit(void) {
     for (uint8_t pin = 0; pin < 32; pin++)
         NRF_P0->PIN_CNF[pin] = GPIO_CNF[pin];
 
-    NRF_P0->OUT = LED_ALL;
-    NRF_P0->DIRSET = LED_ALL;
+    NRF_P0->OUT = LED_ALL | UART_TXD;
+    NRF_P0->DIR = LED_ALL | UART_TXD;
 }
 void ClockInit(void) {
     NRF_CLOCK->LFCLKSRC = CLKSRC_XTAL;
