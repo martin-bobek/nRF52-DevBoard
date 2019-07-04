7bcb25c2d6f017de67a9615a82764b45ac330684
martin-bobek <martinbobek8@gmail.com>
2019-07-03 22:42:24 -0600

    Moved the portmap into a separate file.
    Moved the pin number and mask definitions into a separate header which
    can be included in other parts of the project.

--- header/init.h
+++ header/init.h
@@ -1,26 +1,9 @@
 #ifndef INIT_H__
 #define INIT_H__
 
+#include "portmap.h"
+
 #define MSK(value, shift)   (uint32_t)((value) << (shift))
-#define BIT(n)              MSK(1, n)
-
-#define RTS_PIN         5
-#define TXD_PIN         6
-#define CTS_PIN         7
-#define RXD_PIN         8
-#define LED1_PIN        17
-#define LED2_PIN        18
-#define LED3_PIN        19
-#define LED4_PIN        20
-
-#define UART_RTS        BIT(RTS_PIN)
-#define UART_TXD        BIT(TXD_PIN)
-#define UART_CTS        BIT(CTS_PIN)
-#define UART_RXD        BIT(RXD_PIN)
-#define LED1            BIT(LED1_PIN)
-#define LED2            BIT(LED2_PIN)
-#define LED3            BIT(LED3_PIN)
-#define LED4            BIT(LED4_PIN)
 
 #define LED_ALL         (LED1 | LED2 | LED3 | LED4)
 
