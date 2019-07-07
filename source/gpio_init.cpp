638c2dca41ee4a578a8c717ed57bcf1e36d71cc3
martin-bobek <martinbobek8@gmail.com>
2019-07-06 23:40:40 -0600

    Added an ArrayInit constexpr class.
    The ArrayInit class is used to define arrays where values can be
    assigned to specific indices. All array elements not specifically
    assigned to are given a default value. ArrayInit is intended to define
    constexpr constant arrays.

--- source/gpio_init.cpp
+++ source/gpio_init.cpp
@@ -1,3 +1,5 @@
+#include <cstddef>
+#include <initializer_list>
 #include <nrf.h>
 #include "gpio_init.h"
 #include "init.h"
@@ -32,6 +34,28 @@ static constexpr uint32_t LED_PIN_CNF =
         GPIO_PIN_CNF_PULL_Disabled    << GPIO_PIN_CNF_PULL_Pos  |
         GPIO_PIN_CNF_DRIVE_H0S1       << GPIO_PIN_CNF_DRIVE_Pos |
         GPIO_PIN_CNF_SENSE_Disabled   << GPIO_PIN_CNF_SENSE_Pos;
+
+struct Element {
+public:
+    constexpr Element(size_t _pos, uint32_t _val) : pos(_pos), val(_val) {}
+    size_t pos;
+    uint32_t val;
+};
+template <size_t N, uint32_t Default>
+class ArrayInit {
+public:
+    constexpr ArrayInit(const std::initializer_list<Element> list) : arr() {
+        for (size_t i = 0; i < N; i++)
+            arr[i] = Default;
+
+        for (auto it = list.begin(); it < list.end(); it++)
+            arr[it->pos] = it->val;
+    }
+
+    uint32_t operator[](size_t index) const { return arr[index]; }
+private:
+    uint32_t arr[N];
+};
 
 static const uint32_t GPIO_CNF[32] = {
 //      P0.00 XL1    P0.01 XL2    P0.02        P0.03        P0.04        P0.05 RTS    P0.06 TxD    P0.07 CTS
