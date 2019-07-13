6d4e5c82a3be7a91c902172895501b930fd5dd1c
martin-bobek <martinbobek8@gmail.com>
2019-07-12 21:25:32 -0700

    Slowed down delay loops.
    Increased delay times by a factor of two to match original delay.

--- source/main.cpp
+++ source/main.cpp
@@ -13,12 +13,12 @@ int main() {
 
     NRF_P0->OUTCLR = LED_ALL;
 
-    for (uint8_t i = 0; i < 4; i++, LedsToggle = 0)
+    for (uint8_t i = 0; i < 8; i++, LedsToggle = 0)
         while (!LedsToggle);
 
     NRF_P0->OUTSET = LED_ALL;
 
-    for (uint8_t i = 0; i < 4; i++, LedsToggle = 0)
+    for (uint8_t i = 0; i < 8; i++, LedsToggle = 0)
         while (!LedsToggle);
 
     // RE-ENABLE LEDS TO INDICATE TEST OUTCOME
