2c9a45442016e8c3722edeed7f21cdc968849a36
martin-bobek <martinbobek8@gmail.com>
2019-07-20 22:57:54 -0600

    Modified _exit to place chip in SYSTEMOFF.
    Instead of entering an infinite loop, _exit now shuts down the
    microcontroller, reducing power on exit.

--- source/syscalls.cpp
+++ source/syscalls.cpp
@@ -1,5 +1,11 @@
+#include <nrf.h>
+
 extern "C" void _exit();
 
+static constexpr uint32_t TURN_OFF =
+        POWER_SYSTEMOFF_SYSTEMOFF_Enter << POWER_SYSTEMOFF_SYSTEMOFF_Pos;
+
 void _exit() {
-    while (true);
+    while (true)
+        NRF_POWER->SYSTEMOFF = TURN_OFF;
 }
