d36a2de7235b5b2b50103bef9e0f7cc8ee847f28
martin-bobek <martinbobek8@gmail.com>
2019-07-24 21:42:28 -0600

    Added a SerialWrite function to the UART driver.
    This function adds a pointer to a string to a circular buffer. The
    driver assumes the string will not be modified in the future.

--- header/uarte.h
+++ header/uarte.h
@@ -1,6 +1,10 @@
 #ifndef UARTE_H__
 #define UARTE_H__
 
+constexpr int SERIAL_SUCCESS     =  0;
+constexpr int SERIAL_BUFFER_FULL = -1;
+
 void UartInit();
+int SerialWrite(const char *str);
 
 #endif
--- source/uarte.cpp
+++ source/uarte.cpp
@@ -22,6 +22,14 @@ static constexpr uint32_t UARTE_CONFIG =
 static constexpr uint32_t UARTE_ENABLE =
         UARTE_ENABLE_ENABLE_Enabled << UARTE_ENABLE_ENABLE_Pos;
 
+static constexpr uint32_t BUFFER_LENGTH = 32;
+static constexpr uint32_t BUFFER_FULL 	= BUFFER_LENGTH;
+
+static const char *ptrBuffer[BUFFER_LENGTH];
+static uint8_t head = 0;
+static uint8_t tail = 0;
+
+
 void UartInit() {
     NRF_UARTE0->PSEL.RTS = UARTE_PSEL_RTS;
     NRF_UARTE0->PSEL.TXD = UARTE_PSEL_TXD;
@@ -30,4 +38,19 @@ void UartInit() {
     NRF_UARTE0->BAUDRATE = UARTE_BAUDRATE;
     NRF_UARTE0->CONFIG = UARTE_CONFIG;
     NRF_UARTE0->ENABLE = UARTE_ENABLE;
+}
+
+int SerialWrite(const char *str) {
+    if (head == BUFFER_FULL)
+        return SERIAL_BUFFER_FULL;
+
+    ptrBuffer[head] = str;
+
+    head++;
+    if (head == BUFFER_LENGTH)
+        head = 0;
+    if (head == tail)
+        head = BUFFER_FULL;
+
+    return SERIAL_SUCCESS;
 }
