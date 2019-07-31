d94c3bc19383333b7e0faf26d39eca236801a9cc
martin-bobek <martinbobek8@gmail.com>
2019-07-31 00:02:38 -0600

    Resolved potential race condition.
    Made the BUFFER_LENGTH and BUFFER_FULL constants distinguishable from
    each other, so that these values will not be interpreted as the wrong
    state by another thread.

--- source/uarte.cpp
+++ source/uarte.cpp
@@ -47,7 +47,7 @@ static constexpr uint32_t UARTE_ENABLE =
         UARTE_ENABLE_ENABLE_Enabled << UARTE_ENABLE_ENABLE_Pos;
 
 static constexpr uint32_t BUFFER_LENGTH = 32;
-static constexpr uint32_t BUFFER_FULL 	= BUFFER_LENGTH;
+static constexpr uint32_t BUFFER_FULL 	= BUFFER_LENGTH + 1;
 
 static Message ptrBuffer[BUFFER_LENGTH];
 static volatile uint8_t head = 0;
