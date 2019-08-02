e6204dda71425432650244e115bd6d27d2bc2e00
martin-bobek <martinbobek8@gmail.com>
2019-08-02 02:21:09 -0600

    Implemented syscall kill.
    Copied the suggested minimal implementation in the Red Hat newlib C
    library documentation.

--- source/syscalls.cpp
+++ source/syscalls.cpp
@@ -1,10 +1,15 @@
+#include <cerrno>
 #include <sys/stat.h>
+
+#undef errno
+extern int errno;
 
 extern "C" void _exit();
 extern "C" int _close(int file);
 extern "C" int _fstat(int file, struct stat *st);
 extern "C" int _getpid();
 extern "C" int _isatty(int file);
+extern "C" int _kill(int pid, int sig);
 
 void _exit() {
     while (true);
@@ -25,4 +30,9 @@ int _getpid() {
 
 int _isatty(int) {
     return 1;
+}
+
+int _kill(int, int) {
+    errno = EINVAL;
+    return -1;
 }
