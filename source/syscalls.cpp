89f821ba0e7ff04ab4f3485ad689b6cab6dabc88
martin-bobek <martinbobek8@gmail.com>
2019-08-02 01:55:50 -0600

    Implemented syscall write.
    This implementation expects a complete message string to be passed as an
    argument. The string is then copied over to an internal buffer and
    passed to the serial driver. Subsequent calls will fail until the driver
    releases the buffer. This works poorly with newlib nano, since it
    typically makes multiple single character calls to write.

--- source/syscalls.cpp
+++ source/syscalls.cpp
@@ -1,5 +1,33 @@
+#include <cctype>
+#include <cerrno>
+#include "uarte.h"
+
 extern "C" void _exit();
+extern "C" int _write(int file, char *ptr, int len);
+
+constexpr size_t WRITE_BUFFER_SIZE = 40;
 
 void _exit() {
     while (true);
+}
+
+int _write(int, char *ptr, int len) {
+    static char buffer[WRITE_BUFFER_SIZE];
+    static size_t length = 0;
+
+    if (buffer[length] != SERIAL_RELEASED) {
+        errno = ENOSPC;
+        return -1;
+    }
+
+    size_t index;
+    for (index = 0; index < (size_t)len && index + 1 < WRITE_BUFFER_SIZE; index++)
+        buffer[index] = ptr[index];
+    buffer[index] = SERIAL_UNRELEASED;
+
+    if (SerialWrite(buffer, index) == SERIAL_SUCCESS)
+        return index;
+
+    errno = ENOSPC;
+    return -1;
 }
