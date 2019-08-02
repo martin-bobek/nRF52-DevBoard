c87164eea0d0ee87aca5f79d52a10ca3fbdaf6df
martin-bobek <martinbobek8@gmail.com>
2019-08-02 02:13:47 -0600

    Implemented syscall getpid.
    Copied the suggested minimal implementation in the Red Hat newlib C
    library documentation.

--- source/syscalls.cpp
+++ source/syscalls.cpp
@@ -3,6 +3,7 @@
 extern "C" void _exit();
 extern "C" int _close(int file);
 extern "C" int _fstat(int file, struct stat *st);
+extern "C" int _getpid();
 
 void _exit() {
     while (true);
@@ -15,4 +16,8 @@ int _close(int) {
 int _fstat(int, struct stat *st) {
     st->st_mode = S_IFCHR;
     return 0;
+}
+
+int _getpid() {
+    return 1;
 }
