488300686a5b98607faea886dfe1782d91893d87
martin-bobek <martinbobek8@gmail.com>
2019-08-02 01:55:50 -0600

    Implemented syscall write.
    This implementation expects only calls with single character strings.
    When its internal buffer is filled or a newline character is detected,
    the buffered string is printed out over UART.

--- source/syscalls.cpp
+++ source/syscalls.cpp
@@ -15,6 +15,12 @@ extern "C" int _kill(int pid, int sig);
 extern "C" int _lseek(int file, int ptr, int dir);
 extern "C" int _read(int file, char *ptr, int len);
 extern "C" caddr_t _sbrk(int incr);
+extern "C" int _write(int file, char *ptr, int len);
+
+static constexpr size_t WRITE_BUFFER_SIZE = 100;
+static constexpr char UNSENT = ~0;
+static char LENGTH_ERROR_STR[] = "Error: Call to _write used more than 1 character.";
+static constexpr size_t LENGTH_ERROR_LEN = sizeof(LENGTH_ERROR_STR) - 1;
 
 static char HEAP_OVERFLOW_STR[] = "Error: Heap needs to expand into the stack.";
 static constexpr size_t HEAP_OVERFLOW_LEN = sizeof(HEAP_OVERFLOW_STR) - 1;
@@ -66,4 +72,41 @@ caddr_t _sbrk(int incr) {
 
     heap_end += incr;
     return (caddr_t)prev_heap_end;
+}
+
+int _write(int, char *ptr, int len) {
+    static char buffer[WRITE_BUFFER_SIZE] = { UNSENT };
+    static size_t length = 0;
+
+    if (len != 1) {
+        SerialWrite(LENGTH_ERROR_STR, LENGTH_ERROR_LEN);
+        _exit();
+    }
+    if (buffer[length] == SERIAL_UNRELEASED) {
+        errno = ENOSPC;
+        return -1;
+    }
+    if (buffer[length] == SERIAL_RELEASED)
+        length = 0;
+
+    buffer[length] = *ptr;
+    length++;
+
+    if (*ptr == '\n') {
+        buffer[length] = '\r';
+        length++;
+    }
+
+    if (*ptr == '\n' || length + 2 >= WRITE_BUFFER_SIZE) {
+        buffer[length] = SERIAL_UNRELEASED;
+
+        if (SerialWrite(buffer, length) != SERIAL_SUCCESS) {
+            errno = ENOSPC;
+            return -1;
+        }
+    }
+    else
+        buffer[length] = UNSENT;
+
+    return 1;
 }
